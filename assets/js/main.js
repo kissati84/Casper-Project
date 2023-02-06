85466f74404ebeed7d258fdc9d74a6abe865b1e3
Attila István Kiss <kissati23@gmail.com>
2023-02-06 10:40:26 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById("nav-menu");
+navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav-link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== VIDEOS ====================*/
+document.querySelectorAll('.work-container video').forEach(vid => {
+    vid.onclick = () => {
+        document.querySelector('.popup-video').style.display = 'block';
+        document.querySelector('.popup-video video').src = vid.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-video span').onclick = () =>{
+    document.querySelector('.popup-video').style.display = 'none';
+}
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-up class
+  if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2800,
+  //reset: true,
+})
+
+sr.reveal(`.home-data, .home-social-link, .work-container`,{
+  origin: 'top',
+  interval: 100,
+})
+
+sr.reveal(`.about-data, .contact-content-r`,{
+  origin: 'right',
+})
+
+sr.reveal(`.about-img, .contact-content-l`,{
+  origin: 'left',
+  interval: 100,
+})
+
