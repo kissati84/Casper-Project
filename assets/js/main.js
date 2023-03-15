c8b502b8eef909b836fe5b534d42854fb285374a
Attila István Kiss <kissati23@gmail.com>
2023-03-15 15:08:15 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,60 @@ let swiperThumbs = new Swiper(".gallery-thumbs", {
 
 swiperThumbs.controller.control = swiperCards
 
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  contactProject = document.getElementById("contact-project"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Check if the field has a value
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    contactProject.value === ""
+  ) {
+    // Add and remove color
+    contactMessage.classList.remove("color-blue");
+    contactMessage.classList.add("color-red");
+
+    // Show message
+    contactMessage.textContent = "Írjon az összes mezőbe.";
+  } else {
+    // serviceID - templateID - #form - publicKey
+    emailjs
+      .sendForm(
+        "service_lytd648",
+        "template_ttu95l1",
+        "#contact-form",
+        "-vMhhdP7IZv_7JpT3"
+      )
+      .then(
+        () => {
+          // Show message and add color
+          contactMessage.classList.add("color-blue");
+          contactMessage.textContent = "Üzenet elküldve!";
+
+          // Remove message after five seconds
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("OOOPS! VALAMI BAJ TÖRTÉNT...", error);
+        }
+      );
+
+    // To clear the input field
+    contactName.value = "";
+    contactEmail.value = "";
+    contactProject.value = "";
+  }
+};
+contactForm.addEventListener("submit", sendEmail);
 
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp(){
